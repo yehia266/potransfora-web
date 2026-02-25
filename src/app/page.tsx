7bc9650ad14b7e91e0046926ef2702b1cb05e617
yehia266 <146366869+yehia266@users.noreply.github.com>
2026-02-25 22:46:10 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 export default function Home() {
 return (
 <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black text-black dark:text-white">
+<title>{ "{{ $json.seo_title || 'POTransfora' }}" }</title>
+<meta name="description" content={ "{{ $json.seo_description || 'Sovereign Protocol' }}" } />
 
 );
 }
