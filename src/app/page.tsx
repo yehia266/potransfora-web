a6b20ea011caf7fd1a357bdc99b14610911a343c
yehia266 <146366869+yehia266@users.noreply.github.com>
2026-02-25 22:41:00 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
 return (
-<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-<title>{ "POTransfora | Algorithmic Justice & Digital Sovereignty Prot" }</title>
-<meta name="description" content={ "• Leading the future with the Organizational DNA Protocol and Cyber-Human Immunity. Discover the Sovereign Treasury and the Immortal Digital Mentor within POTra" } />
+<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black text-black dark:text-white">
 
 );
 }
