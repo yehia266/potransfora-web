b8cf9ebb38624881ebce428a34f4b2d03d608c8e
yehia266 <146366869+yehia266@users.noreply.github.com>
2026-03-04 22:53:42 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,18 +2,15 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
       },
-      letterSpacing: {
-        tightest: '-.075em',
+      colors: {
+        foundation: '#F8FAFC',
       }
     },
   },
