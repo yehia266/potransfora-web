13009fd330a86b3dd9648da746c8cbad17aff1ba
yehia266 <146366869+yehia266@users.noreply.github.com>
2026-03-16 01:22:55 +0200

    Update page.tsx

--- src/app/plasmic-host/page.tsx
+++ src/app/plasmic-host/page.tsx
@@ -1,4 +1,4 @@
-import { PLASMIC } from "@/plasmic-init";
+import { PLASMIC } from "../../plasmic-init"; // لاحظ النقطتين مرتين للخروج من app و plasmic-host
 import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
 
 export default function PlasmicHost() {
