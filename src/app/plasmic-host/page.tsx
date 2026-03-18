3f8b1508b2aa97c86295083872302f93e050e5b4
yehia266 <146366869+yehia266@users.noreply.github.com>
2026-03-18 22:42:07 +0200

    Update page.tsx

--- src/app/plasmic-host/page.tsx
+++ src/app/plasmic-host/page.tsx
@@ -1,6 +1,22 @@
-import { PLASMIC } from "../../plasmic-init"; // لاحظ النقطتين مرتين للخروج من app و plasmic-host
-import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+import { EyeModal } from "./components/EyeModal";
 
-export default function PlasmicHost() {
-  return <PlasmicCanvasHost />;
-}
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: "k7tGuJznBEQcX8ySStbZUu",
+      token: "YESOdQOKFw7rt7VUwVMX4y27akAxU5iNWNdFdir3uzOKo5BqJXCtseaLJ2mqZTZ1oyPnpPIWDX9qJYuizA",
+    },
+  ],
+  preview: true,
+});
+
+// تسجيل المكون بالأسم الصحيح للبحث
+PLASMIC.registerComponent(EyeModal, {
+  name: "EyeModal",
+  props: {
+    title: "string",
+    description: "string",
+    className: "string"
+  },
+});
